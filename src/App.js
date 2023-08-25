696c246a370ca1deeee3f777de2871fa973a29eb
Mahsood Ebrahim <mahsoodebrahim@gmail.com>
2023-08-24 19:45:38 -0700

    changed app text

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Change made on the feature branch
         </a>
       </header>
     </div>
